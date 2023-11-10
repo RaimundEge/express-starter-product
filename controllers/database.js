6232092a6478be0341b02c1cb41b0ca93a2b955f
Marceline Gallegos <marceline.gallegos@icloud.com>
2023-11-10 10:37:42 -0600

    Fix "SQLITE_CANTOPEN" error by adjusting file path

--- controllers/database.js
+++ controllers/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // open the database
-let db = new sqlite3.Database('../db/database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
   if (err) {
     console.error(err.message);
   }
